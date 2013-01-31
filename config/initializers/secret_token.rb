6ca75d2420c5e3289a5d90236463198769e1f354
Daniel Benami <danielbenamis@gmail.com>
2013-01-31 13:23:09 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cmuch::Application.config.secret_token = '06ce2f18271fa3305e83b08fc42a8d9663eca132eeee0db0fc71ade905c8e30dfd5b8272370b3ef0df279f014ab770e3388e77ecfe1aa3bd68d4180f42d60d68'
