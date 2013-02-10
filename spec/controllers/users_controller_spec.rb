ba1971fbd03b18235e7e76b4ed6d6fa2d174314b
Daniel Benami <danielbenamis@gmail.com>
2013-02-10 21:56:26 +0200

    Initial interface for following/followers

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -114,6 +114,15 @@ render_views
       get :show, :id => @user      
       response.should have_selector('td.sidebar', :content => @user.microposts.count.to_s)
     end
+
+    describe "when signed in as another user" do
+      it "should be successful" do
+        test_sign_in(FactoryGirl.create(:user, :email => FactoryGirl.generate(:email)))
+        get :show, :id => @user
+        response.should be_success
+      end
+    end
+
   end
 
   describe "GET 'new'" do
